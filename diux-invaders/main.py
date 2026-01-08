711473ecd8aa2c6d82daa7e50efc750f222b06f5
Jesus Figueroa <diuxddr@gmail.com>
2026-01-08 09:52:31 -0800

    Added player and input handling

--- diux-invaders/main.py
+++ diux-invaders/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(F"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,20 +9,29 @@ def main():
     print(F"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
-
-        clock = pygame.time.Clock()
-        dt = 0
 
         # Get out of the game loop if needed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
+            
+        # Update
+        player.update(dt)
 
+        # Render
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Wait
         dt = clock.tick(60) / 1000
+        print(F"Update dt:{dt}")
 
 if __name__ == "__main__":
     main()
