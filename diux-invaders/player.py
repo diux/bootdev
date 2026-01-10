97b3c36f732c5baca0820501bd0913c196bc4ecb
Jesus Figueroa <diuxddr@gmail.com>
2026-01-10 15:31:13 -0800

    movement
    movement

--- diux-invaders/player.py
+++ diux-invaders/player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED,  LINE_WIDTH
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED,  LINE_WIDTH
 
 class Player(CircleShape):
 
@@ -18,7 +18,12 @@ class Player(CircleShape):
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-    
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        self.position += rotated_vector * PLAYER_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
@@ -29,3 +34,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
